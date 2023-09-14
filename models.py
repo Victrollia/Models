e1212c70c76c97a8f426e59ea2d739b5c3aea57d
Victoria Cadogan <45957062+Victrollia@users.noreply.github.com>
2023-09-14 12:02:05 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-user_list = []
 while True:
-	input_string = input('Enter elements of a list separated by newline. When done, enter "q": ')
-	user_list.append(input_string[4:])
-	if input_string == 'q':
-		#user_list.remove('q')
-		print(','.join(user_list)[1:-1])
-		break
-# print list
+	user_list = []
+	print('\n Enter elements of a list separated by newline. When done, enter "q": ')
+	while True:
+		input_string = input(">")
+		user_list.append(input_string[4:])
+		if input_string == 'q':
+			#user_list.remove('q')
+			print('\n')
+			print(','.join(user_list)[1:-1])
+	# print list
